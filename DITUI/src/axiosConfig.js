474c4d57c0641c40f51bc0117454438a50b2d158
titusdev <titusowuor30@gmail.com>
2023-10-29 16:00:02 +0300

    updates ui env

--- DITUI/src/axiosConfig.js
+++ DITUI/src/axiosConfig.js
@@ -1,9 +1,10 @@
 import axios from "axios"
 
 const tokenString = localStorage.getItem('token')
+const baseURL = process.env.VUE_APP_BASE_URL || 'http://localhost:8001/api/'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8001/api/', // Replace with your Django backend URL
+  baseURL: baseURL, // Replace with your Django backend URL
   timeout: 360_000, // 6 mins timeout
   headers: {
     'Content-Type': 'application/json',
